0c5caa585ebc4f428ab0bb1185fc8a07e54b33ed
Barry <a255667313@gmail.com>
2023-10-31 18:00:16 +0800

    变更1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Barry
+ * @project Algorithm
+ * @date 31/10/2023
+ */
+public class Main {
+	public static void main( String[] args ) {
+		System.out.println("Hello world!");
+	}
+}
